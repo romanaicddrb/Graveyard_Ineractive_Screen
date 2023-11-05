84b281ddb41e41844dc185ab33d7f815a32b8db7
unknown <icddrb@LT-5229.icddrb.net>
2023-11-05 11:32:56 +0600

    project create

---
+++ src/main/java/com/graveyard/InteractiveScreenApplication.java
@@ -0,0 +1,13 @@
+package com.graveyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveScreenApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(InteractiveScreenApplication.class, args);
+    }
+
+}
